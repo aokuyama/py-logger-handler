b0d573b5d62870741667422b1753b35e0b3dc04d
aokuyama <ao980s@gmail.com>
2021-11-02 22:07:29 +0900

    packaging

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+ 
+setuptools.setup(
+    name="logger_handler",
+    version="0.0.1",
+    author="aokuyama",
+    author_email="mail@aokuyama.work",
+    description="logger handler",
+    long_description="logger handler",
+    long_description_content_type="text/markdown",
+    url="https://web.aokuyama.work",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
